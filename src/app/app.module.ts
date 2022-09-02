34880d90e1bc44b57b65e59c17517e97e0cae1ef
YasinKavuk <yasinkavuk100@gmail.com>
2022-09-02 18:02:51 +0200

    gridview basic
    gridview with 4 view layout. Will probably be changed again later.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridViewComponent } from './components/grid-view/grid-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
