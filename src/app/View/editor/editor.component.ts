4a52b8f619b9fa81319f3894a2bb1a25641df805
Max Brockmann <max@tbrockmann.de>
2022-09-16 16:42:02 +0200

    added custom Syntax Highlighting for Ace-Editor

--- src/app/View/editor/editor.component.ts
+++ src/app/View/editor/editor.component.ts
@@ -1,7 +1,13 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import { MacroProviderService } from "src/app/Controller/macro-provider.service";
 
 import * as ace from "ace-builds";
-import { MacroProviderService } from "src/app/Controller/macro-provider.service";
+
+
+const LANG  = "ace/mode/mic1";
+const THEME = "ace/theme/gruvbox";
+
+
 
 @Component({
   selector: "app-editor",
@@ -22,15 +28,23 @@ export class EditorComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
 
-    ace.config.set("fontSize", "14px");
-    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
+    ace.config.set("basePath", "/assets/editor")
+
+    const editorOptions: Partial<ace.Ace.EditorOptions> = {
+      highlightActiveLine: true,
+      minLines: 20,
+      fontSize: 14,
+      autoScrollEditorIntoView: true,
+      useWorker: false,
+      
+    }
 
     // create Ace.Editor Object
-    this.aceEditor = ace.edit(this.editor.nativeElement);
+    this.aceEditor = ace.edit(this.editor.nativeElement, editorOptions);
 
     this.aceEditor.session.setValue(this.content);
-    this.aceEditor.setTheme("ace/theme/gruvbox");
-    this.aceEditor.session.setMode('ace/mode/javascript');
+    this.aceEditor.setTheme(THEME);
+    this.aceEditor.session.setMode(LANG);
 
     this.aceEditor.on("input", () =>{
       this.content = this.aceEditor.getValue();
