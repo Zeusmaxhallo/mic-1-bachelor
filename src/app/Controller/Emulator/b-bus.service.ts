509b09054f2ee2cd5b33d5ab080746cc49f4f5b0
Mango <efumango@gmail.com>
2022-09-27 17:33:01 +0200

    update b-bus

--- src/app/Controller/Emulator/b-bus.service.ts
+++ src/app/Controller/Emulator/b-bus.service.ts
@@ -7,14 +7,23 @@ import { RegProviderService } from '../reg-provider.service';
 
 export class BBusService {
   private regProviderService: RegProviderService;
-  private regName: string;
 
   constructor(regProviderService: RegProviderService) {
     this.regProviderService = regProviderService;
    }
-
-  getValue(): number{
-    return this.regProviderService.getRegister(this.regName).getValue();
-  }  
-
+  
+  getValue(regNum: number): number{
+    switch(regNum){
+      case 0: return this.regProviderService.getRegister("MDR").getValue();
+      case 1: return this.regProviderService.getRegister("PC").getValue();
+      case 2: return this.regProviderService.getRegister("MBR").getValue();
+      case 3: return this.regProviderService.getRegister("MBRU").getValue();
+      case 4: return this.regProviderService.getRegister("SP").getValue();
+      case 5: return this.regProviderService.getRegister("LV").getValue();
+      case 6: return this.regProviderService.getRegister("CPP").getValue();
+      case 7: return this.regProviderService.getRegister("TOS").getValue();
+      case 8: return this.regProviderService.getRegister("OPC").getValue();
+      default: return null;
+    }
+  } 
 }
