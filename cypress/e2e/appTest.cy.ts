489ff1f65ff057ae20d252dea9740ba824b3a623
YasinKavuk <yasinkavuk100@gmail.com>
2023-02-10 16:03:22 +0100

    Issue #53: demo code app workflow test, navigation to default page test, persisttance of macro and micro code test

--- cypress/e2e/appTest.cy.ts
+++ cypress/e2e/appTest.cy.ts
@@ -1,42 +1,65 @@
-describe('complete workflow test', () => {
-  it('Test animation and demo code 1', {defaultCommandTimeout: 120000}, () => {
+describe('complete workflow test without animation', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('Test the navigation to the default page', () => {
+    cy.url().should('include', '/')
+  });
+
+  it('tests whether the macro- and micro-code is persistant when the site is reloaded', () => {
+    cy.emptyEditors();
+    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(2) > div > div > app-micro-editor > div')
+      .type('testMicro7457034950', {delay: 1})
+      .should('include.text', 'testMicro7457034950');
+    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
+      .type('testMacro9349856039', {delay: 1})
+      .should('include.text', 'testMacro9349856039');
     cy.visit('/');
-    cy.url().should('include', '/');
+    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(2) > div > div > app-micro-editor > div')
+      .should('include.text', 'testMicro7457034950');
+    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
+      .should('include.text', 'testMacro9349856039');
+  });
+
+  it('Test demo code 1', {defaultCommandTimeout: 120000}, () => {
+    cy.emptyEditors();
+    cy.getStandardMicroPrograms();
     cy.getDemoCode1(); // writes demo code to editor. There are also more demo code that can be used. For example getDemoCode3().
     cy.pushLoadButton();
-    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(1) > div > div > app-tool-bar-mic-view > div > div > mat-slider')
-      .type("{end}");
+    cy.get('#mat-checkbox-1-input').uncheck({force: true});
     cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(1) > div > div > app-tool-bar-mic-view > section > button:nth-child(2) > span.mat-button-wrapper')
       .click();
     cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(4) > div > div > app-stack > div > div:nth-child(8) > div.stack-value.ng-tns-c71-0')
       .should('have.text', ' 15 ');
   });
 
-  it('Test animation and demo code 2(uses custom microprograms) ', {defaultCommandTimeout: 120000}, () => {
-    cy.visit('/');
-    cy.url().should('include', '/');
-    cy.getDemoMicroProgram();
+  it('Test demo code 2(uses custom microprograms) ', {defaultCommandTimeout: 10000}, () => {
+    cy.emptyEditors();
+    cy.getDemoMicroPrograms();
     cy.getDemoCode2(); // writes demo code to editor. There are also more demo code that can be used. For example getDemoCode3().
     cy.pushLoadButton();
-    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(1) > div > div > app-tool-bar-mic-view > div > div > mat-slider')
-      .type("{end}");
+    cy.get('#mat-checkbox-1-input').uncheck({force: true});
     cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(1) > div > div > app-tool-bar-mic-view > section > button:nth-child(2) > span.mat-button-wrapper')
       .click();
     cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(4) > div > div > app-stack > div > div:nth-child(8) > div.stack-value.ng-tns-c71-0')
       .should('have.text', ' 15 ');
   });
 
-  it('Test animation and the demo code 3(uses jumps)', {defaultCommandTimeout: 120000}, () => {
-    cy.visit('/');
-    cy.url().should('include', '/');
+  it('Test demo code 3(uses jumps)', {defaultCommandTimeout: 10000}, () => {
+    cy.emptyEditors();
+    cy.getStandardMicroPrograms();
     cy.getDemoCode3(); // writes demo code to editor. There are also more demo code that can be used. For example getDemoCode3().
     cy.pushLoadButton();
-    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(1) > div > div > app-tool-bar-mic-view > div > div > mat-slider')
-      .type("{end}");
+    cy.get('#mat-checkbox-1-input').uncheck({force: true});
     cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(1) > div > div > app-tool-bar-mic-view > section > button:nth-child(2) > span.mat-button-wrapper')
       .click();
-    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(4) > div > div > app-stack > div > div:nth-child(8) > div.stack-value.ng-tns-c71-0')
+    cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(4) > div > div > app-stack > div > div:nth-child(9) > div.stack-value.ng-tns-c71-0')
       .should('have.text', ' 8 ');
+  });
+
+  it('Test the persistance of user input when site is reloaded', () => {
+
   });
 
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -46,6 +46,129 @@ const microCode: string = `Main1: PC=PC+1; fetch; goto(MBR)
 
 (0x00)NOP:; goto Main1
 
+(0x10)BIPUSH: SP=MAR=SP+1;
+PC=PC+1; fetch
+MDR=TOS=MBR; wr; goto Main1;
+
+(0x13)ISUB: MAR=SP=SP-1; rd
+H=TOS
+MDR=TOS=MDR-H; wr; goto Main1
+
+(0x16)IAND: MAR=SP=SP-1; rd
+H=TOS
+MDR=TOS=MDR AND H; wr; goto Main1
+
+(0x02)IOR:MAR=SP=SP-1; rd
+H=TOS
+MDR=TOS=MDR OR H; wr; goto Main1
+
+(0x05)DUP: MAR = SP = SP+1
+MDR=TOS;wr;goto Main1
+(0x57)POP: MAR=SP=SP-1; rd;
+TOS=MDR;goto Main1
+
+(0x19)SWAP: MAR=SP-1; rd;
+MAR=SP
+H=MDR;wr
+MDR=TOS
+MAR=SP-1; wr
+TOS=H; goto Main1
+
+(0x0D)IADD: MAR=SP=SP-1; rd
+H=TOS
+MDR=TOS=MDR+H; wr; goto Main1
+
+(0x1F)ILOAD:H=LV;
+MAR=MBRU+H;rd;
+iload3: MAR=SP=SP+1;
+PC=PC+1; fetch; wr;
+TOS=MDR; goto Main1
+
+(0x24)ISTORE: H=LV
+MAR=MBRU+H
+istore3: MDR=TOS;wr;
+SP=MAR=SP-1;rd
+PC=PC+1;fetch
+TOS=MDR; goto Main1
+
+(0x2A)WIDE: PC=PC+1; fetch; goto(MBR or 0x100)
+
+(0x2B)LDC_W: PC=PC+1; fetch;
+H=MBRU <<8
+H=MBRU OR H
+MAR=H+CPP; rd; goto iload3
+
+(0x2F)IINC: H=LV
+MAR=MBRU+H; rd
+PC=PC+1; fetch
+H=MDR
+PC=PC+1; fetch
+MDR=MBR+H; wr; goto Main1
+
+(0xA7)GOTO:OPC=PC-1
+goto2: PC=PC+1; fetch;
+H=MBR <<8
+H=MBRU OR H
+PC=OPC+H; fetch
+goto Main1
+
+(0x09)IFLT: MAR=SP=SP-1; rd;
+OPC=TOS
+TOS=MDR
+N=OPC; if(N) goto T; else goto F
+
+(0x35)IFEQ:MAR=SP=SP-1; rd;
+OPC=TOS
+TOS=MDR
+Z=OPC; if(Z) goto T; else goto F
+
+(0x39)IF_ICMPEQ: MAR=SP=SP-1; rd
+MAR=SP=SP-1
+H=MDR;rd
+OPC=TOS
+TOS=MDR
+Z=OPC-H; if(Z) goto T; else goto F
+F:PC=PC+1
+PC=PC+1; fetch;
+goto Main1
+
+(0x13F)T:OPC=PC-1;fetch; goto goto2
+
+(0xB6)INVOKEVIRTUAL:PC=PC+1; fetch
+H=MBRU <<8
+H = MBRU OR H
+MAR=CPP + H; rd
+OPC = PC+1
+PC=MDR; fetch
+PC=PC+1; fetch
+H = MBRU <<8
+H = MBRU OR H
+PC=PC+1; fetch
+TOS=SP-H
+TOS=MAR=TOS+1
+PC=PC+1; fetch
+H = MBRU <<8
+H = MBRU OR H
+MDR = SP + H +1; wr
+MAR=SP=MDR
+MDR = OPC; wr
+MAR = SP = SP+1
+MDR = LV; wr
+PC=PC+1; fetch
+LV=TOS; goto Main1
+
+(0xAF)IRETURN:MAR=SP=LV; rd
+LV=MAR=MDR; rd
+MAR=LV+1
+PC=MDR;rd;fetch
+MAR=SP
+LV=MDR
+MDR=TOS; wr; goto Main1`
+
+const customMicroCode: string = `Main1: PC=PC+1; fetch; goto(MBR)
+
+(0x00)NOP:; goto Main1
+
 (0xC6)BIPUSH: SP=MAR=SP+1;
 PC=PC+1; fetch
 MDR=TOS=MBR; wr; goto Main1;
@@ -169,49 +292,66 @@ MAR=SP
 LV=MDR
 MDR=TOS; wr; goto Main1`;
 
+
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     pushLoadButton(): typeof pushLoadButton;
-    getDemoCode3(): typeof getDemoCode3;
     getDemoCode1(): typeof getDemoCode1;
     getDemoCode2(): typeof getDemoCode2;
-    getDemoMicroProgram(): typeof getDemoMicroProgram;
+    getDemoCode3(): typeof getDemoCode3;
+    getDemoMicroPrograms(): typeof getDemoMicroPrograms;
+    getStandardMicroPrograms(): typeof getStandardMicroPrograms;
+    emptyEditors(): typeof emptyEditors;
   }
 }
+
 
 function pushLoadButton(): void{
   cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > button > span.mat-button-wrapper')
     .click();
 }
 
-function getDemoCode3(): void {
-  cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
-    .type(code3);
-}
-
 function getDemoCode1(): void {
   cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
-    .type(code1);
+    .type(code1, {delay: 1});
 }
 
 function getDemoCode2(): void {
   cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
-    .type(code2);
+    .type(code2, {delay: 1});
 }
 
-function getDemoMicroProgram(): void {
+function getDemoCode3(): void {
+  cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
+    .type(code3, {delay: 1});
+}
+
+function getDemoMicroPrograms(): void {
   cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(2) > div > div > app-micro-editor > div')
-    .type('{selectall}{selectall}{backspace}' + microCode);
+    .type(customMicroCode, {delay: 1});
+}
+
+function getStandardMicroPrograms(): void {
+  cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(2) > div > div > app-micro-editor > div')
+    .type(microCode, {delay: 1});
+}
+
+function emptyEditors(): void {
+  cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(3) > div > div > app-editor > div')
+    .type('{selectall}{selectall}{selectall}{backspace}');
+  cy.get('body > app-root > app-grid-view > mat-grid-list > div > mat-grid-tile:nth-child(2) > div > div > app-micro-editor > div')
+    .type('{selectall}{selectall}{selectall}{backspace}');
 }
 
 
-//
 // NOTE: You can use it like so:
-Cypress.Commands.add('getDemoCode3', getDemoCode3);
 Cypress.Commands.add('getDemoCode1', getDemoCode1);
 Cypress.Commands.add('getDemoCode2', getDemoCode2);
+Cypress.Commands.add('getDemoCode3', getDemoCode3);
 Cypress.Commands.add('pushLoadButton', pushLoadButton);
-Cypress.Commands.add('getDemoMicroProgram', getDemoMicroProgram);
+Cypress.Commands.add('getDemoMicroPrograms', getDemoMicroPrograms);
+Cypress.Commands.add('getStandardMicroPrograms', getStandardMicroPrograms);
+Cypress.Commands.add('emptyEditors', emptyEditors);
 //
 // ***********************************************
 // This example commands.js shows you how to
