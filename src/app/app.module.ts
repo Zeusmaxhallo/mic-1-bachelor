b8171d77f7e3749d2e7f68577c01dd62f5f4afc8
Max Brockmann <max@tbrockmann.de>
2023-02-17 20:56:46 +0100

    closes #78

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { ToolBarComponent } from './View/tool-bar/tool-bar.component';
 import {MatButtonModule} from '@angular/material/button';
 import { ToolBarMicViewComponent } from './View/tool-bar-mic-view/tool-bar-mic-view.component';
-import { RegisterComponent } from './View/register/register.component';
 import { EditorComponent } from './View/editor/editor.component';
 import {TextFieldModule} from '@angular/cdk/text-field';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import * as FileSaver from 'file-saver';
 import { StackComponent } from './View/stack/stack.component';
 import { MicroEditorComponent } from './View/micro-editor/micro-editor.component';
 import { MicVisualizationComponent } from './View/mic-visualization/mic-visualization.component';
@@ -36,7 +34,6 @@ import { RegistersComponent } from './View/SVG/registers/registers.component';
     GridViewComponent,
     ToolBarComponent,
     ToolBarMicViewComponent,
-    RegisterComponent,
     EditorComponent,
     StackComponent,
     MicroEditorComponent,
