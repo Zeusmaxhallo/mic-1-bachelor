c2672509901c90c19131fbd6c34941dbcd01efd1
Max Brockmann <max@tbrockmann.de>
2023-06-21 10:44:21 +0200

    Added N and Z bit Vis; closes #149

--- src/app/View/SVG/alu-flag-arrows/alu-flag-arrows.component.ts
+++ src/app/View/SVG/alu-flag-arrows/alu-flag-arrows.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { DirectorService } from 'src/app/Controller/director.service';
+import { PresentationModeControllerService } from 'src/app/Controller/presentation-mode-controller.service';
 
 
 @Component({
@@ -13,14 +14,29 @@ export class AluFlagArrowsComponent implements AfterViewInit {
   public zFlag = false;
   public nFlag = false;
 
+  public presentationMode = false;
+
   constructor(
     private director: DirectorService,
+    private presentationModeController: PresentationModeControllerService,
   ) { }
 
   ngAfterViewInit(): void {
     this.director.aluFlags$.subscribe(flags => {
       this.zFlag = flags.Z;
       this.nFlag = flags.N;
+
+      const zRegister = document.querySelector(".Z_Value");
+      zRegister.innerHTML = this.zFlag ? "1" : "0";
+
+      const nRegister = document.querySelector(".N_Value");
+      nRegister.innerHTML = this.nFlag ? "1" : "0";
+
+    });
+
+
+    this.presentationModeController.presentationMode$.subscribe( mode => {
+      this.presentationMode = mode.presentationMode;
     })
   }
 
