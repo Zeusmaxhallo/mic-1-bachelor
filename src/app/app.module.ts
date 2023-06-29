f5a86bad3c81597849d59213852f5cccd41049fa
Max Brockmann <max@tbrockmann.de>
2023-06-29 20:54:37 +0200

    Added Canvas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ import { DebugConsoleComponent } from './View/debug-console/debug-console.compon
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AluFlagArrowsComponent } from './View/SVG/alu-flag-arrows/alu-flag-arrows.component';
 import { MemoryViewComponent } from './View/memory-view/memory-view.component';
-import { ScreenComponent } from './GrapicAdapter/screen/screen.component';
+import { ScreenComponent } from './GraphicsAdapter/screen/screen.component';
 
 
 @NgModule({
