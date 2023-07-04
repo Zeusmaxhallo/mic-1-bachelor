1d12a66153bd7230af6d2c4decf2c13c016a36f6
Max Brockmann <max@tbrockmann.de>
2023-07-04 12:03:45 +0200

    removed Brackets in first level node

--- src/app/GraphicsAdapter/screen/screen.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { ScreenComponent } from './screen.component'
-
-describe('ScreenComponent', () => {
-  it('should mount', () => {
-    cy.mount(ScreenComponent)
-  })
-})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AluFlagArrowsComponent } from './View/SVG/alu-flag-arrows/alu-flag-arrows.component';
 import { MemoryViewComponent } from './View/memory-view/memory-view.component';
 import {MatTreeModule} from '@angular/material/tree';
-import { ScreenComponent } from './GraphicsAdapter/screen/screen.component';
 
 
 @NgModule({
@@ -59,7 +58,6 @@ import { ScreenComponent } from './GraphicsAdapter/screen/screen.component';
     DebugConsoleComponent,
     AluFlagArrowsComponent,
     MemoryViewComponent,
-    ScreenComponent,
   ],
   imports: [
     BrowserModule,
